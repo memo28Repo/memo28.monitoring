8e0657054f63eac6cb575120828358f69aea2762
memo.repo <1r56955415918@qq.com>
2024-01-17 11:58:35 +0800

    :bug:fix(monitoring-platform): 优化后台详情展示字段

--- apps/monitoring-platform/src/pages/home/errorDetails.tsx
+++ apps/monitoring-platform/src/pages/home/errorDetails.tsx
@@ -1,5 +1,5 @@
 import {useParams} from "react-router-dom";
-import {Typography, Divider, Form, Tag} from 'antd'
+import {Typography, Divider, Form, Tag, Descriptions} from 'antd'
 import {getErrorLog} from '@memo28.monitoring/service'
 import type {errorLogDetails} from '@memo28.monitoring/service'
 import {useEffect, useState} from "react";
@@ -30,52 +30,106 @@ const ErrorDetails = () => {
             <Tag className="ml-4" color="#2db7f5">{details.kind}</Tag>
             <img className="h-5 w-5 flex mr-2" src={getErrorLogIcon(details.type || '')} alt=""/>
         </div>
-        <Divider orientation="left">错误信息</Divider>
-        <div className={'text-red-700 font-bold mt-2'}>
-            {details.message || 'no message'}
-        </div>
-        <Form.Item label={'错误时间'}>
-            {dayjs(details.CreatedAt).format("YYYY-MM-DD HH:mm:ss")}
-        </Form.Item>
 
-        <Form.Item label={'报错堆栈'}>
-            <SyntaxHighlighter language={'json'} style={docco} children={details.stack || ''}/>
-        </Form.Item>
+        <Descriptions title={'错误信息'} layout={'vertical'} items={[
+            {
+                key: 1,
+                label: '错误信息',
+                children: <>
+                    {details.message || 'no message'}
+                </>
+            },
+            {
+                key: 2,
+                label: '错误时间',
+                children: <>
+                    {dayjs(details.CreatedAt).format("YYYY-MM-DD HH:mm:ss")}
+                </>
+            },
+            {
+                key: 3,
+                label: '代码行列',
+                children: <>
+                    {details.colno || 0}:{details.lineno || 0}
+                </>
+            },
+            {
+                key: 4,
+                label: '报错堆栈',
+                children: <>
+                    <SyntaxHighlighter language={'json'} style={docco} children={details.stack || ''}/>
+                </>
+            }
+        ]}/>
 
-        <Form.Item label={'代码行列'}>
-            {details.colno || 0}:{details.lineno || 0}
-        </Form.Item>
+        <Descriptions title={'用户信息'} layout="vertical" items={[
+            {
+                key: 1,
+                label: '设备宽度',
+                children: <>
+                    {details.screenWidth || 'unknown'}
+                </>
+            },
+            {
+                key: 2,
+                label: '设备高度',
+                children: <>
+                    {details.screenHeight || 'unknown'}
+                </>
+            },
+            {
+                key: 3,
+                label: '设备方向',
+                children: <>
+                    {details.deviceOrientation || 'unknown'}
+                </>
+            },
+            {
+                key: 4,
+                label: '浏览器语言',
+                children: <>
+                    {details.browserLanguage || 'unknown'}
+                </>
+            },
+            {
+                key: 5,
+                label: '用户信息',
+                children: <>
+                    {details.userAgent || 'unknown'}
+                </>
+            },
+        ]}/>
 
-        <Divider orientation="left">用户信息</Divider>
-        <Form.Item label={'设备宽度'}>
-            {details.screenWidth || 'unknown'}
-        </Form.Item>
-        <Form.Item label={'设备高度'}>
-            {details.screenHeight || 'unknown'}
-        </Form.Item>
-        <Form.Item label={'设备方向'}>
-            {details.deviceOrientation || 'unknown'}
-        </Form.Item>
-        <Form.Item label={'浏览器语言'}>
-            {details.browserLanguage || 'unknown'}
-        </Form.Item>
-        <Form.Item label={'用户信息'}>
-            {details.userAgent || 'unknown'}
-        </Form.Item>
-
-        <Divider orientation="left">网络信息</Divider>
-        <Form.Item label={'下行带宽(downLink)'}>
-            {details.downLink || 'unknown'}(Mbit/s)
-        </Form.Item>
-        <Form.Item label={'往返时延(rtt)'} extra={<>表示设备与服务器之间的往返时延，以毫秒为单位</>}>
-            {details.downLink || 'unknown'}(ms)
-        </Form.Item>
-        <Form.Item label={'数据节省模式(saveData)'} extra={<>表示用户代理是否启用了数据节省模式，以毫秒为单位</>}>
-            {details.saveData || 'unknown'}(ms)
-        </Form.Item>
-        <Form.Item label={'网络类型(effectiveType)'} extra={<>表示用户代理是否启用了数据节省模式，以毫秒为单位</>}>
-            {details.effectiveType || 'unknown'}(ms)
-        </Form.Item>
+        <Descriptions title={'网络信息'} layout="vertical" items={[
+            {
+                key: 1,
+                label: '下行带宽(downLink)',
+                children: <>
+                    {details.downLink || 'unknown'}(Mbit/s)
+                </>
+            },
+            {
+                key: 2,
+                label: '往返时延(rtt) - 表示设备与服务器之间的往返时延，以毫秒为单位',
+                children: <>
+                    {details.rtt || 'unknown'}(ms)
+                </>
+            },
+            {
+                key: 3,
+                label: '数据节省模式(saveData) - 表示用户代理是否启用了数据节省模式，以毫秒为单位',
+                children: <>
+                    {details.saveData || 'unknown'}(ms)
+                </>
+            },
+            {
+                key: 4,
+                label: '网络类型(effectiveType) - 表示用户代理是否启用了数据节省模式，以毫秒为单位',
+                children: <>
+                    {details.effectiveType || 'unknown'}(ms)
+                </>
+            }
+        ]}/>
     </>
 }
 
